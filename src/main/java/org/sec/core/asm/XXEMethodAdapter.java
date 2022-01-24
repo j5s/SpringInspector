ff1b5427c9ba6af864a3c2aabcdeb15b147c572f
EmYiQing <2023503307@qq.com>
2022-01-24 14:27:54 +0800

    XXE

--- src/main/java/org/sec/core/asm/XXEMethodAdapter.java
+++ src/main/java/org/sec/core/asm/XXEMethodAdapter.java
@@ -48,10 +48,20 @@ public class XXEMethodAdapter extends CoreMethodAdapter<Boolean> {
                 desc.equals("(Ljava/lang/String;)V");
         boolean inputSourceCondition = owner.equals("org/xml/sax/InputSource") && name.equals("<init>") &&
                 desc.equals("(Ljava/io/InputStream;)V");
+        boolean streamSourceCondition = owner.equals("javax/xml/transform/stream/StreamSource") &&
+                name.equals("<init>");
 
         boolean saxBuilderCondition = owner.equals("org/jdom2/input/SAXBuilder") && name.equals("build");
+        boolean saxParserCondition = owner.equals("javax/xml/parsers/SAXParser") && name.equals("parse");
+        boolean saxTransformerFactoryCondition = owner.equals("javax/xml/transform/sax/SAXTransformerFactory") &&
+                name.equals("newTransformerHandler");
+        boolean schemaFactoryCondition = owner.equals("javax/xml/validation/SchemaFactory") &&
+                name.equals("newSchema");
+        boolean transformerFactory = owner.equals("javax/xml/transform/Transformer") && name.equals("transform");
+        boolean validatorCondition = owner.equals("javax/xml/validation/Validator") && name.equals("validate");
+        boolean xmlReaderCondition = owner.equals("org/xml/sax/XMLReader") && name.equals("parse");
 
-        if (fileCondition || fileInputStreamCondition || inputSourceCondition) {
+        if (fileCondition || fileInputStreamCondition || inputSourceCondition || streamSourceCondition) {
             if (operandStack.get(0).contains(true)) {
                 super.visitMethodInsn(opcode, owner, name, desc, itf);
                 operandStack.set(0, true);
@@ -61,6 +71,48 @@ public class XXEMethodAdapter extends CoreMethodAdapter<Boolean> {
         if (saxBuilderCondition) {
             if (operandStack.get(0).contains(true)) {
                 pass.put("SAX-BUILDER", true);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                return;
+            }
+        }
+        if (saxParserCondition) {
+            if (operandStack.get(1).contains(true)) {
+                pass.put("SAX-PARSER", true);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                return;
+            }
+        }
+        if (saxTransformerFactoryCondition) {
+            if (operandStack.get(0).contains(true)) {
+                pass.put("SAX-TRANSFORMER-FACTORY", true);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                return;
+            }
+        }
+        if (schemaFactoryCondition) {
+            if (operandStack.get(0).contains(true)) {
+                pass.put("SCHEMA-FACTORY", true);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                return;
+            }
+        }
+        if (transformerFactory) {
+            if (operandStack.get(1).contains(true)) {
+                pass.put("TRANSFORMER-FACTORY", true);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                return;
+            }
+        }
+        if (validatorCondition) {
+            if (operandStack.get(0).contains(true)) {
+                pass.put("VALIDATOR", true);
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
+                return;
+            }
+        }
+        if (xmlReaderCondition) {
+            if (operandStack.get(0).contains(true)) {
+                pass.put("XML-READER", true);
                 super.visitMethodInsn(opcode, owner, name, desc, itf);
                 return;
             }
--- src/main/java/org/sec/core/service/XXEService.java
+++ src/main/java/org/sec/core/service/XXEService.java
@@ -101,6 +101,65 @@ public class XXEService {
                 logger.info("detect sax builder xxe");
                 System.out.println(resultInfo);
             }
+            if (cv.getPass("SAX-PARSER") != null && cv.getPass("SAX-PARSER")) {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setRisk(ResultInfo.MID_RISK);
+                resultInfo.setVulName("SAX PARSER XXE");
+                resultInfo.getChains().addAll(tempChain);
+                results.add(resultInfo);
+                logger.info("detect sax parser xxe");
+                System.out.println(resultInfo);
+            }
+            if (cv.getPass("SAX-TRANSFORMER-FACTORY") != null &&
+                    cv.getPass("SAX-TRANSFORMER-FACTORY")) {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setRisk(ResultInfo.MID_RISK);
+                resultInfo.setVulName("SAX TRANSFORMER FACTORY XXE");
+                resultInfo.getChains().addAll(tempChain);
+                results.add(resultInfo);
+                logger.info("detect sax transformer factory xxe");
+                System.out.println(resultInfo);
+            }
+            if (cv.getPass("SCHEMA-FACTORY") != null &&
+                    cv.getPass("SCHEMA-FACTORY")) {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setRisk(ResultInfo.MID_RISK);
+                resultInfo.setVulName("SCHEMA FACTORY XXE");
+                resultInfo.getChains().addAll(tempChain);
+                results.add(resultInfo);
+                logger.info("detect schema factory xxe");
+                System.out.println(resultInfo);
+            }
+            if (cv.getPass("TRANSFORMER-FACTORY") != null &&
+                    cv.getPass("TRANSFORMER-FACTORY")) {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setRisk(ResultInfo.MID_RISK);
+                resultInfo.setVulName("TRANSFORMER FACTORY XXE");
+                resultInfo.getChains().addAll(tempChain);
+                results.add(resultInfo);
+                logger.info("detect transformer factory xxe");
+                System.out.println(resultInfo);
+            }
+            if (cv.getPass("VALIDATOR") != null &&
+                    cv.getPass("VALIDATOR")) {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setRisk(ResultInfo.MID_RISK);
+                resultInfo.setVulName("VALIDATOR XXE");
+                resultInfo.getChains().addAll(tempChain);
+                results.add(resultInfo);
+                logger.info("detect validator xxe");
+                System.out.println(resultInfo);
+            }
+            if (cv.getPass("XML-READER") != null &&
+                    cv.getPass("XML-READER")) {
+                ResultInfo resultInfo = new ResultInfo();
+                resultInfo.setRisk(ResultInfo.MID_RISK);
+                resultInfo.setVulName("XML READER XXE");
+                resultInfo.getChains().addAll(tempChain);
+                results.add(resultInfo);
+                logger.info("detect xml reader xxe");
+                System.out.println(resultInfo);
+            }
         } catch (Exception e) {
             e.printStackTrace();
             return;
